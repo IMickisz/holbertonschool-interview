a0b11937915926af0ba71159256be757faf7c96f
IMickisz <ivan.mickisz3@gmail.com>
2023-01-26 14:13:14 +0100

    added tasks 0

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script that reads stdin line by line and compute metrics """
+import sys
+
+
+STATUS = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0,
+          '500': 0}
+
+total_size = 0
+n = 0
+try:
+    for line in sys.stdin:
+        args = line.split(" ")
+        if len(args) > 2:
+            status = args[-2]
+            file_size = int(args[-1])
+            if status in STATUS:
+                STATUS[status] += 1
+            total_size += file_size
+            n += 1
+            if n % 10:
+                print("File size: {:d}".format(total_size))
+                for key, value in sorted(STATUS.items()):
+                    if value != 0:
+                        print("{}: {:d}".format(key, value))
+except KeyboardInterrupt:
+    print("File size: {:d}".format(total_size))
+    for key, value in sorted(STATUS.items()):
+        if value != 0:
+            print("{}: {:d}".format(key, value))
