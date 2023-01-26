b3aded3ceeb24ccd4d5b7ac77ede6d32ee1609a5
IMickisz <ivan.mickisz3@gmail.com>
2023-01-26 23:42:14 +0100

    corrected errors for number of status

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,27 +5,28 @@ import sys
 
 lap = 0
 total_size = 0
-STATUS = {'200': 0, '301': 0, '400': 0, '401': 0,
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
+
 
 def print_stats():
     """ Prints statistics from the beginning """
     print("File size: {}".format(total_size))
-    for key in sorted(STATUS.keys()):
-        if STATUS[key] > 0:
-            print("{}: {}".format(key, STATUS[key]))
+    for key, value in sorted(status.items()):
+        if value > 0:
+            print("{}: {}".format(key, status[key]))
 
 
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
-            lap += 1
             try:
+                lap += 1
                 args = line.split()
                 total_size += int(args[-1])
-                status = int(args[-2])
-                if status in STATUS.keys():
-                    STATUS[status] += 1
+                code = args[-2]
+                if code in status:
+                    status[code] += 1
             except ValueError:
                 pass
             if lap % 10 == 0:
