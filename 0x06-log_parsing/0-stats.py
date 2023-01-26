0d7918ef7458cab4f468321a127025eb57719add
IMickisz <ivan.mickisz3@gmail.com>
2023-01-26 23:22:00 +0100

    corrected task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,28 +3,34 @@
 import sys
 
 
-STATUS = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0,
-          '500': 0}
-
+lap = 0
 total_size = 0
-n = 0
-try:
-    for line in sys.stdin:
-        args = line.split(" ")
-        if len(args) > 2:
-            status = args[-2]
-            file_size = int(args[-1])
-            if status in STATUS:
-                STATUS[status] += 1
-            total_size += file_size
-            n += 1
-            if n % 10:
-                print("File size: {:d}".format(total_size))
-                for key, value in sorted(STATUS.items()):
-                    if value != 0:
-                        print("{}: {:d}".format(key, value))
-except KeyboardInterrupt:
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(STATUS.items()):
-        if value != 0:
-            print("{}: {:d}".format(key, value))
+STATUS = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+
+def print_stats():
+    """ Prints statistics from the beginning """
+    print("File size: {}".format(total_size))
+    for key in sorted(STATUS.keys()):
+        if STATUS[key] > 0:
+            print("{}: {}".format(key, STATUS[key]))
+
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            lap += 1
+            try:
+                args = line.split()
+                total_size += int(args[-1])
+                status = int(args[-2])
+                if status in STATUS.keys():
+                    STATUS[status] += 1
+            except ValueError:
+                pass
+            if lap % 10 == 0:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
